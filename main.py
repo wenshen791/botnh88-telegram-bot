c6b6df0c40e8c0ad08d85fe6fab196238f803039
wenshen791 <wshen69vn@gmail.com>
2025-04-07 16:38:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# Thay token bot của bạn ở dòng dưới
-TOKEN = "7645007167:AAEMhmRo1LEmEJp6BJt_tO6-9hKj5yPlVy8"
+TOKEN = '7645007167:AAEMhmRo1LEmEJp6BJt_tO6-9hKj5yPlVy8'
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def welcome(message):
+def send_welcome(message):
     markup = InlineKeyboardMarkup()
     button = InlineKeyboardButton("ĐĂNG KÝ +888K", url="https://nh88trangchu.com")
     markup.add(button)
-    bot.send_message(message.chat.id, "🎁 Chào mừng đến với NH88!\nNhấn nút bên dưới để nhận ngay +888K!", reply_markup=markup)
+    bot.send_message(message.chat.id, "🎉 Nhấn vào nút bên dưới để nhận ngay +888K!", reply_markup=markup)
 
 if __name__ == '__main__':
-    print("🤖 Bot NH88 đang chạy bằng polling...")
+    print("Bot NH88 đang chạy...")
     bot.polling(non_stop=True)
