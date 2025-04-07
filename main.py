61f3d190ee2033087e48d9a7dd24352cba98e3e1
wenshen791 <wshen69vn@gmail.com>
2025-04-07 17:06:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import telebot
 from telebot.types import MenuButtonWebApp, WebAppInfo
 
-# Token NH88 bạn cung cấp
 TOKEN = "7645007167:AAEMhmRo1LEmEJp6BJt_tO6-9hKj5yPlVy8"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
 def welcome(message):
     # Gửi tin nhắn khi user bắt đầu
     bot.send_message(message.chat.id, "🎁 Chào mừng bạn đến với NH88!\nNhấn nút bên dưới để nhận +888K.")
 
-    # Gắn Mini App (Web App Button) ngay dưới khung nhập
     bot.set_chat_menu_button(
         menu_button=MenuButtonWebApp(
             text="🎯 ĐĂNG KÝ +888K",
